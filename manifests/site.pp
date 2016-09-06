0dcaeebc4421ebd359856859c0d742bea5bc1583
Akila Perera <ravihansa3000@gmail.com>
2016-09-06 16:17:47 +0530

    Improve Vagrantfile to expect PUPPET_HOME and HIERA_HOME, update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,5 @@
+# ------------------------------------------------------------------------------
+#
 # Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +13,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# ------------------------------------------------------------------------------
 
-include wso2base::java
-# include wso2as
-# Class['::wso2base::java'] -> Class['::wso2as']
+
+# Include classes - search for classes in *.yaml/*.json files
+if $::use_hieradata {
+  require wso2base::java
+  hiera_include('classes')
+}
+
+node /as\.dev\.wso2\.org/ {
+  include wso2base::java
+  include wso2as
+  Class[wso2base::java] -> Class[wso2as]
+}
