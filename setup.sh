6cf75a229436dde659502db26e42bef7321fb999
Vishanth <vishanth911@gmail.com>
2016-09-23 14:22:53 +0530

    adding checking out branch support

--- setup.sh
+++ setup.sh
@@ -52,11 +52,14 @@ function showUsageAndExit() {
   echo
   echo -en "  -p\t"
   echo "[REQUIRED] Comma separated list of product codes. [esb,is,apim,das][all]"
-  echo -en "  -p\t"
+  echo -en "  -l\t"
   echo "[OPTIONAL] Platform to setup Hiera data. If none given 'default' platform will be taken"
+  echo -en "  -v\t"
+  echo "[OPTIONAL] Product version. If none given latest version will be taken. Multiple products not supported."
   echo
 
   echoBold "Ex: ./setup.sh -p esb "
+  echoBold "Ex: ./setup.sh -p esb -v 4.9.0 "
   echoBold "Ex: ./setup.sh -p esb,apim -l kubernetes"
   echoBold "Ex: ./setup.sh -p all "
   echo
@@ -74,13 +77,48 @@ function validatePuppetHome() {
   fi
 }
 
+# To checkout the git branch and switch between branches if it exists
+# $1 - Puppet module name (equivalent to product name)
+# $2 - product version
+function checkoutBranch() {
+  pushd ${PUPPET_HOME}/modules/${1}
+    if [[ ${2} == "latest" ]]; then
+      if [[ `git branch --list master` ]]; then
+        if [[ `git symbolic-ref --short -q HEAD` != "master" ]]; then
+          echo "checking out master..."
+          git checkout master
+        fi
+      else
+        echo "creating branch master"
+        git checkout -b master
+      fi
+    else
+      if [[ `git branch --list v${2}` ]]; then
+        if [[ `git symbolic-ref --short -q HEAD` != "${2}" ]]; then
+          echo "checking out branch ${2}"
+          git checkout v${2}
+        fi
+      else
+        echo "creating branch ${2}"
+        git checkout -b v${2} origin/v${2} || echoWarn "Puppet module for ${1} ${2} is not available"
+      fi
+    fi
+  popd
+}
+
 # Setup Puppet module for given wso2 product
 # $1 - Puppet module name (equivalent to product name)
 # $2 - product code
 # $3 - platform
+# $4 - product version
 function setupModule() {
   echoInfo "Setting up ${1} Puppet module for ${3} platform..."
   if [ -d "${PUPPET_HOME}/modules/${1}" ]; then
+
+    if [[ ${1} != "wso2base" ]];then
+      checkoutBranch ${1} ${4}
+    fi
+
     echoWarn "${PUPPET_HOME}/modules/${1} directory exists. Skipping..."
     return
   fi
@@ -93,14 +131,16 @@ function setupModule() {
     echoError "Failed to setup PUPPET_HOME"
     exit 1
   fi
+
   git clone ${puppet_git_url} "${PUPPET_HOME}/modules/${1}"
-  # TODO: Checkout released product version tag.
 
   echoInfo "Creating symlink for Hiera data..."
   if [[ ${1} == "wso2base" ]];then
     ln -sf  "${PUPPET_HOME}/modules/${1}/hieradata/dev/wso2/common.yaml" "${PUPPET_HOME}/hieradata/dev/wso2/"
     echoSuccess "Successfully installed ${1} puppet module and Hiera data for ${3} platform."
     return
+  else
+    checkoutBranch ${1} ${4}
   fi
 
   if [[ ${3} == "default" ]]; then
@@ -122,13 +162,16 @@ function setupModule() {
 }
 
 platform='default'
-while getopts :p:l: FLAG; do
+while getopts :p:l:v: FLAG; do
   case ${FLAG} in
     p)
       product_codes=$OPTARG
       ;;
     l)
       platform=$OPTARG
+      ;;
+    v)
+      product_version=$OPTARG
       ;;
     \?)
       showUsageAndExit
@@ -138,6 +181,10 @@ done
 
 if [[ -z ${product_codes} ]]; then
   showUsageAndExit
+fi
+
+if [[ -z ${product_version} ]]; then
+  product_version="latest"
 fi
 
 if [ -z "$PUPPET_HOME" ]; then
@@ -174,7 +221,7 @@ fi
 
 for product_code in "${product_code_array[@]}"; do
   getProductName ${product_code}
-  setupModule ${product_name} ${product_code} ${platform}
+  setupModule ${product_name} ${product_code} ${platform} ${product_version}
 done
 
 echoInfo "Setup completed successfully. Please copy relevant distributions to Puppet file bucket."
